751b28d5a3ab2ca420c455b463862001ae985058
fcanalesp <fccp@protonmail.com>
2019-04-08 16:11:01 -0400

    corregi descifrado

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 //BOTON CIFRAR
 document.getElementById("botonCifrar").onclick = () => {
   let texto = document.getElementById("textoEntrada").value; // creando variable donde se guardara texto ingresado
-  //console.log(texto); // Probando
+  // console.log(texto); // Probando
   let desplazamiento = document.getElementById("clave").value; //creando variable que guardara la clave
   //console.log(desplazamiento);// Probando
-  let show = window.cipher.encode(desplazamiento, texto); // conectando con el archivo js
+  let show = window.cipher.encode(texto, desplazamiento); // conectando con el archivo js
   //console.log(show);
   document.getElementById("outputText").value = show;
 }
@@ -18,9 +18,12 @@ document.getElementById("botonDescifrar").onclick = () => {
   //console.log(desplazamiento); //Pprobando
 
   // Conectando con cioher.js
-  let show = window.cipher.decode(desplazamiento, texto);
+  let show = window.cipher.decode(texto, desplazamiento);
   //console.log(show);
 
   document.getElementById("outputText").value = show;
-
 }
+// BOTON LIMPIAR
+// document.getElementById("limpiar").onclick = () => {//addEventListener("click", function(){
+//   //location.reload();
+// }

834049f86651e84169745a1db6c5220ebd1ce576
fcanalesp <fccp@protonmail.com>
2019-04-08 15:18:12 -0400

    Realice los test

--- src/cipher.js
+++ src/cipher.js
@@ -1,17 +1,17 @@
 window.cipher = {
 
-  encode: (desplazamiento, texto) => {
+  encode: (texto, desplazamiento) => {
     let deNumeroLetra = "";
     for (let i = 0; i < texto.length; i++) { // el for recorre cosas string, array, objetos, watevah!
       let deLetraNumero = texto.charCodeAt(i); //pasando de letra a numero
       let resultado = "";
       if (deLetraNumero >= 65 && deLetraNumero <= 90) { //  Limitando mayusculas
         resultado = (deLetraNumero - 65 + parseInt(desplazamiento)) % 26 + 65;// obteniendo el numero con codigo mayusculas
-        console.log(resultado)// Probando
+        //console.log(resultado)// Probando
       }
       else if (deLetraNumero >= 97 && deLetraNumero <= 122) { // limitando minusculas
         resultado = (deLetraNumero - 97 + parseInt(desplazamiento)) % 26 + 97; // obteniendo codigo de minusculas 
-        console.log(resultado) // Probando
+        //console.log(resultado) // Probando
       }
       else if (deLetraNumero == 32) { // me respete el espacio que el usuario me de.
         resultado = (deLetraNumero);
@@ -21,7 +21,7 @@ window.cipher = {
     return deNumeroLetra;
   },
 
-  decode: (desplazamiento, texto) => {
+  decode: (texto, desplazamiento) => {
 
     let deNumeroLetra = "";
     for (let i = 0; i < texto.length; i++) { // for recorre texto ingresado
@@ -29,11 +29,11 @@ window.cipher = {
       let resultadoDescifrar = ""; // Pasando de numero a letra 
       if (deLetraNumero >= 65 && deLetraNumero <= 90) {// Limitando caracteres mayusculas
         resultadoDescifrar = (deLetraNumero - 90 - parseInt(desplazamiento)) % 26 + 90;// obteniendo el numero con codigo mayusculas
-        console.log(resultadoDescifrar)// Probando
+        //console.log(resultadoDescifrar)// Probando
       }
       else if (deLetraNumero >= 97 && deLetraNumero <= 122) {// Lmitando caracteres minusculas
         resultadoDescifrar = (deLetraNumero - 122 - parseInt(desplazamiento)) % 26 + 122; // obteniendo codigo de minusculas 
-        console.log(resultadoDescifrar)
+        //console.log(resultadoDescifrar)
       }
       else if (deLetraNumero == 32) { // Me respecte el espacio que el usuario me de.
         resultadoDescifrar = (deLetraNumero);
--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-//window.onload = () => {
 
 //BOTON CIFRAR
 document.getElementById("botonCifrar").onclick = () => {
-  let texto = document.getElementById("textoEntrada").value;// creando variable donde se guardara texto ingresado
-  console.log(texto); // Probando
-  let desplazamiento = document.getElementById("clave").value;//creando variable que guardara la clave
-  console.log(desplazamiento);// Probando
+  let texto = document.getElementById("textoEntrada").value; // creando variable donde se guardara texto ingresado
+  //console.log(texto); // Probando
+  let desplazamiento = document.getElementById("clave").value; //creando variable que guardara la clave
+  //console.log(desplazamiento);// Probando
   let show = window.cipher.encode(desplazamiento, texto); // conectando con el archivo js
-  console.log(show);
+  //console.log(show);
   document.getElementById("outputText").value = show;
 }
 
 //BOTON DESCIFRAR
 document.getElementById("botonDescifrar").onclick = () => {
   let texto = document.getElementById("textoEntrada").value; //variable que guarda texto
-  console.log(texto); // probando
+  //console.log(texto); // probando
   let desplazamiento = document.getElementById("clave").value; // variable que guarda la clave
-  console.log(desplazamiento); //Pprobando
+  //console.log(desplazamiento); //Pprobando
 
   // Conectando con cioher.js
   let show = window.cipher.decode(desplazamiento, texto);
-  console.log(show);
+  //console.log(show);
 
   document.getElementById("outputText").value = show;
 
 }
-
